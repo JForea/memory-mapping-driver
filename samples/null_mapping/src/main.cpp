1ef167b355126c177d9022fe86e5899ba996408e
JForea <ngrigorenko655@gmail.com>
2026-08-24 13:48:04 +0300

    feat: add simple memory allocation test

---
+++ samples/null_mapping/src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include <mmd/mmd.h>
+
+int main() {
+    int *val = nullptr;
+    int err = mem_allocate(reinterpret_cast<unsigned long>(val), sizeof(int));
+    if (err < 0) {
+        std::cout << "Something went wrong.\n";
+        return err;
+    }
+
+    *val = 220;
+
+    std::cout << *val << '\n';
+
+    mem_free(reinterpret_cast<unsigned long>(val), sizeof(int));
+}
