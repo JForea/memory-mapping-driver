2e0a2a387c188fae3916793d4d08e6b0a6db8ea5
JForea <ngrigorenko655@gmail.com>
2026-08-26 11:37:20 +0300

    feat: add multiple page allocation test sample

---
+++ samples/mp_alloc_test/src/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#include <mmd/mmd.h>
+#include <unistd.h>
+
+int main() {
+    int pageSize = getpagesize();
+    int allocSize = pageSize * 2;
+
+    int *val = nullptr;
+    int err = mem_allocate(reinterpret_cast<unsigned long>(val), allocSize);
+    if (err < 0) {
+        std::cout << "Something went wrong: " << err << '\n';
+        return err;
+    }
+
+    int cnt = pageSize * 2 / sizeof(int);
+    
+    const int printPerLine = 10;
+    int currentlyPrinted = 0;
+    while (cnt--) {
+        *val = cnt;
+
+        if (currentlyPrinted == printPerLine) {
+            std::cout << std::endl;
+            currentlyPrinted = 0;
+        }
+
+        std::cout << *val << ' ';
+        currentlyPrinted++;
+
+        val++;
+    }
+
+    mem_free(reinterpret_cast<unsigned long>(nullptr), allocSize);
+}
